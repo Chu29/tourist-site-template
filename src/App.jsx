949a44d378b40c77acd2a402876184b1ea3f8abb
Chu A. <chu.amk22@gmail.com>
2025-12-01 10:58:54 +0100

    chore(setup): install dependencies and setup page routes
    setup tailwindcss and react-route, configure all page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import PackagesPage from "./pages/PackagesPage";
+import GalleryPage from "./pages/GalleryPage";
+import ContactUsPage from "./pages/ContactUsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/about-us" Component={AboutUsPage} />
+        <Route path="/our-packages" Component={PackagesPage} />
+        <Route path="/gallery" Component={GalleryPage} />
+        <Route path="/contact-us" Component={ContactUsPage} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
